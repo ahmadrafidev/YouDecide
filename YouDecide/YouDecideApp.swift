cf1b143a6c8891cb18f43f3e8606f8c7ae185ad3
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-23 21:24:50 +0700

    Initial Commit

---
+++ YouDecide/YouDecideApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouDecideApp.swift
+//  YouDecide
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct YouDecideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
